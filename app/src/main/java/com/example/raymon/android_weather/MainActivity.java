40d8e7a33a92289d2c43c310846b5c4c5904306f
Zhemin Su <raymondsu2010@hotmail.com>
2017-11-05 17:00:30 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/raymon/android_weather/MainActivity.java
+++ app/src/main/java/com/example/raymon/android_weather/MainActivity.java
@@ -260,11 +260,12 @@ public class MainActivity extends AppCompatActivity implements PlaceSelectionLis
             //Log.i("update_forecast","min: "+min_temp+" max: "+max_temp+" weather: "+weather_status);
         }
 
+        //first
         CityData cityData = new CityData();
         cityData.addForecast(temp_list);
 
         List<Temp_Weather> temp_list_forcast = new ArrayList<>();
-        for(int i = 3 ;i<40;i+=8)
+        for(int i = 2 ;i<list.length();i+=7)
         {
             JSONObject temp_object = list.getJSONObject(i).getJSONObject("main");
             JSONObject temp_array = list.getJSONObject(i).getJSONArray("weather").getJSONObject(0);
